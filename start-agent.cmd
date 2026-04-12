d7c48f84ec0517d64fb49cd40098ce23479076ab
Gao Jiaxin <gaojiaxin@local>
2026-04-13 00:30:29 +0800

    chore: pin start scripts to JDK8 and Maven

--- start-agent.cmd
+++ start-agent.cmd
@@ -2,4 +2,7 @@
 setlocal enabledelayedexpansion
 cd /d %~dp0
 if not exist logs mkdir logs
+set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-8.0.482.8-hotspot"
+set "MAVEN_HOME=G:\apache-maven-3.9.14-bin\apache-maven-3.9.14"
+set "PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PATH%"
 call mvn -q -pl data-foundry-agent-service -DskipTests spring-boot:run 1>> logs\agent.out.log 2>> logs\agent.err.log
